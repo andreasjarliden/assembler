057e88151139fbf6d6fd6b532705fdd3e47e23c2
Andreas Järliden <andreas@littlemarauder.com>
2015-02-26 11:24:26 +0100

    Add ability to specify input file on command line (without piping it in)

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 #include "Commands.hpp"
 #include "Assembler.hpp"
 #include "MachineCode.hpp"
+#include <cassert>
+#include <cstdio>
+#include <cstdlib>
 
 extern "C" {
 
@@ -12,7 +15,18 @@ Assembler ASSEMBLER;
 Commands& COMMANDS = ASSEMBLER;
 const char* filename = "STDIN";
 
-int main() {
+int main(int argc, const char* argv[]) {
+  if (argc > 1) {
+    extern FILE* yyin;
+    FILE* f = fopen(argv[1], "r");
+    if (!f) {
+      fprintf(stderr, "fatal error: could not open input file %s\n", argv[1]);
+      exit(-1);
+    }
+    assert(f);
+    filename = argv[1];
+    yyin = f;
+  }
   yyparse();
   ASSEMBLER.machineCode().printHex();
   return 0;
