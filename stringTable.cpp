81c405f26577ad09b5926e8d665cfb4379b4b184
Andreas Järliden <andreas@littlemarauder.com>
2015-12-02 16:04:14 +0100

    Add string escapes for newline and tab

--- stringTable.cpp
+++ stringTable.cpp
@@ -17,13 +17,29 @@ private:
 
 SymbolTable SYMBOL_TABLE;
 
+std::string replaced(std::string s, const std::string& from, const std::string& to) {
+  size_t startIndex = s.find(from);
+  while (startIndex != std::string::npos) {
+    s = s.replace(startIndex, from.length(), to);
+    startIndex = s.find(from);
+  }
+  return s;
 }
+
+std::string replacedEscapes(std::string s) {
+  s = replaced(s, "\\n", "\n");
+  s = replaced(s, "\\t", "\t");
+  return s;
+}
+
+} // anonymous namespace
 
 const char* addSymbol(const char* symbol) {
   return SYMBOL_TABLE.add(symbol);
 }
 
 const char* addString(const char* s) {
-  std::string strippedQuotes(s + 1, strlen(s) - 2);
-  return SYMBOL_TABLE.add(strippedQuotes);
+  const std::string strippedQuotes(s + 1, strlen(s) - 2);
+  const std::string processedString(replacedEscapes(strippedQuotes));
+  return SYMBOL_TABLE.add(processedString);
 }
